a527e92b474f3d08b87b5420e0984f211cf7106b
Jovan Djordjevic <joca100799@gmail.com>
2024-05-07 18:29:57 +0200

    napravljen templejt

---
+++ twitter_clone/resources/views/terms.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+
+@section('content')
+<h1>Terms</h1>
+<div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ducimus sunt nesciunt quibusdam dolore. Minima fugit voluptatum omnis pariatur molestiae iure, laboriosam iste quo nesciunt ut officiis nulla obcaecati perspiciatis!
+</div>
+@endsection
